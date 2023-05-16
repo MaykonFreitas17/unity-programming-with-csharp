c141857d105e1bf65062adbda0ba05e6c794fffd
Maykon Freitas <maykon17programmer@gmail.com>
2023-05-16 16:14:12 -0300

    feat: adiciona arquivo sobre Laço de Repetição - Foreach

--- learning-csharp/Assets/Scripts/Foreach.cs
+++ learning-csharp/Assets/Scripts/Foreach.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class Foreach : MonoBehaviour
 {
+    public string[] Enemies = new string[3];
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Definindo o elementos do array Enemies
+        Enemies[0] = "Goblin lvl 1";
+        Enemies[1] = "Goblin lvl 2";
+        Enemies[2] = "Goblin lvl 3";
+
+        foreach(string enemy in Enemies)
+        {
+            Debug.Log(enemy);
+        }
     }
 
     // Update is called once per frame
